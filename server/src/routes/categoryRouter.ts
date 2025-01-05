089cfca4b3b659feceb82cdd2c020b90b73d4980
FazeElian <elian.ibarra@soy.sena.edu.co>
2025-01-05 16:53:01 -0500

    add: Server validation to create category

--- server/src/routes/categoryRouter.ts
+++ server/src/routes/categoryRouter.ts
@@ -1,10 +1,19 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { CategoryController } from "../controllers/CategoryController";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router()
 
 router.get("/categories/", CategoryController.getAll)
-router.post("/categories/create", CategoryController.create)
+
+router.post("/categories/create",
+    body("name")
+        .notEmpty().withMessage("The category name is required"),
+    handleInputErrors,
+    CategoryController.create
+);
+
 router.get("/categories/:id", CategoryController.getById)
 router.put("/categories/:id", CategoryController.updateById)
 router.delete("/categories/:id", CategoryController.deleteById)
