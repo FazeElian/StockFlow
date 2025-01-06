403ec88b55a9a53146f20cbdda6cb83a25a27bad
FazeElian <elian.ibarra@soy.sena.edu.co>
2025-01-06 10:17:22 -0500

    add: send confirmation email function

--- server/src/controllers/AuthController.ts
+++ server/src/controllers/AuthController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import User from '../models/User';
 import { hashPassword } from '../utils/auth';
 import { generateToken } from '../utils/token';
+import { AuthEmail } from '../emails/AuthEmail';
 
 export class AuthController {
     static register = async (req: Request, res: Response) => {
@@ -21,6 +22,12 @@ export class AuthController {
             user.password = await hashPassword(password)
             user.token = generateToken()
             await user.save();
+
+            await AuthEmail.sendConfirmationEmail({
+                userName: user.userName,
+                email: user.email,
+                token: user.token
+            })
 
             res.status(201).send("Your account has been successfully created.");
         } catch (error) {
