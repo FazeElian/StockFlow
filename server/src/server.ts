f1664d2e35934255b71e99541b9609112972a228
FazeElian <elian.ibarra@soy.sena.edu.co>
2025-01-06 10:52:27 -0500

    add: requests limiter

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import colors from "colors";
 import morgan from 'morgan'
 import { db } from './config/db'
+import { limiter } from './config/limiter';
 
 // Routers
 import categoryRouter from "./routes/categoryRouter";
@@ -22,6 +23,7 @@ const app = express()
 
 app.use(morgan('dev'))
 app.use(express.json())
+app.use(limiter)
 
 // API
 app.use("/api/", categoryRouter);
