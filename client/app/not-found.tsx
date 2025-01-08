38270eb876c2f0b1aa24f6ca826bba87e93a02a3
FazeElian <elian.ibarra@soy.sena.edu.co>
2025-01-08 11:43:01 -0500

    add: Error 404 - Not found view

--- client/app/not-found.tsx
+++ client/app/not-found.tsx
@@ -1,8 +1,32 @@
+"use client";
+
+import Link from "next/link";
+
+// Styles for this component
+import "@/public/css/components/NotFound.css";
+
+// Framer motion
+import { motion } from "framer-motion";
+
 export default function NotFound() {
     return (
-        <div>
-            <h1>Not Found</h1>
-            <h2>Error 404</h2>
-        </div>
+        <main className="content-page--company font-inter">
+            <motion.div
+                className="not-found"
+                initial={{ opacity: 0, scale: 0 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                    duration: 0.8,
+                    delay: .3,
+                    ease: [0, .71, .2, 1.01]
+                }}
+            >
+                <h1>404</h1>
+                <h2>Page not Found</h2>
+                <Link href="/" className="btn-not-found">
+                    Back to Home
+                </Link>
+            </motion.div>
+        </main>
     )
 }
