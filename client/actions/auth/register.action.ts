107ca270d68054ca09071a80f727bb41a828c859
FazeElian <elian.ibarra@soy.sena.edu.co>
2025-01-11 13:52:23 -0500

    Getting validation error messages

--- client/actions/auth/register.action.ts
+++ client/actions/auth/register.action.ts
@@ -1,5 +1,21 @@
 "use server"
 
+import { RegisterSchema } from "@/schemas"
+
 export async function register(userData: FormData) {
-    
+    // Data from form
+    const registerData = {
+        username: userData.get("username"),
+        email: userData.get("email"),
+        password: userData.get("password"),
+    }
+
+    // Validation
+    const register = RegisterSchema.safeParse(registerData);
+
+    const errors = register.error?.errors.map(error => error.message);
+    console.log(errors)
+
+    // New
+    console.log(register)
 }
