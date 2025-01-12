6e002a1237a592430fe5a03dcdabdba678daecf1
FazeElian <elian.ibarra@soy.sena.edu.co>
2025-01-12 13:16:16 -0500

    add: Confirm account function finished

--- client/src/views/company/auth/ConfirmAccountView.tsx
+++ client/src/views/company/auth/ConfirmAccountView.tsx
@@ -1,4 +1,7 @@
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { isAxiosError } from "axios";
+import { toast, Toaster } from "sonner";
 
 // Styles
 import "../../../../public/css/components/company/auth/Forms.css";
@@ -12,71 +15,98 @@ import { ErrorMessageValidation } from "../../../components/company/auth/ErrorMe
 // Type
 import { ConfirmAccountForm } from "../../../types/auth";
 
+// API call
+import api from "../../../config/axios";
+
 const ConfirmAccountView = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm<ConfirmAccountForm> ({
+    const { register, handleSubmit, reset, formState: { errors } } = useForm<ConfirmAccountForm> ({
         defaultValues: {
             token: undefined
         }
     })
 
+    const navigate = useNavigate();
+
     const handleConfirmAccount = async (formData: ConfirmAccountForm) => {
-        console.log(formData)
+        try {
+            const { data } = await api.post("/auth/confirm-account", formData);
+            reset();
+
+            // Sucess message
+            toast.success(data)
+
+            setTimeout(() => {
+                // Redirection to login form
+                navigate("/auth/login");
+            }, 2000)
+        } catch (error) {
+            if (isAxiosError(error) && error.response) {
+                toast.error(error.response.data.error);
+            }
+        }
     }
 
     return (
-        <main className="content-page--company">
-            <section className="sect-form-users">
-                <form
-                    action=""
-                    className="form-users bg-black-medium font-inter"
-                    method="post"
-                    onSubmit={handleSubmit(handleConfirmAccount)}
-                >
-                    <div className="top-form-users bg-transparent">
-                        <img
-                            src={Logo}
-                            className="bg-transparent"
-                            alt=""
-                            loading="lazy"
-                        />
-                        <h2 className="color-gray bg-transparent">
-                            Confirm your account using the code sent to your email
-                        </h2>
-                    </div>
-                    <div className="inputs-form-users bg-transparent">
-                        <div className="group-form-users bg-transparent">
-                            <label htmlFor="token" className="bg-transparent color-white">Confirmation Code</label>
-                            <input
-                                type="text"
-                                id="token"
-                                className="color-black bg-white font-inter"
-                                placeholder="Enter the code sent to your email"
-                                {...register("token", {
-                                    required: "Token not valid",
-                                    minLength: {
-                                        value: 6,
-                                        message: "Token not valid"
-                                    },
-                                    maxLength: {
-                                        value: 6,
-                                        message: "Token not valid"
-                                    }
-                                })}
+        <>
+            <main className="content-page--company">
+                <section className="sect-form-users">
+                    <form
+                        action=""
+                        className="form-users bg-black-medium font-inter"
+                        method="post"
+                        onSubmit={handleSubmit(handleConfirmAccount)}
+                    >
+                        <div className="top-form-users bg-transparent">
+                            <img
+                                src={Logo}
+                                className="bg-transparent"
+                                alt=""
+                                loading="lazy"
                             />
-                            {errors.token && 
-                                <ErrorMessageValidation>
-                                    { errors.token?.message }
-                                </ErrorMessageValidation>
-                            }
+                            <h2 className="color-gray bg-transparent">
+                                Confirm your account using the code sent to your email
+                            </h2>
                         </div>
+                        <div className="inputs-form-users bg-transparent">
+                            <div className="group-form-users bg-transparent">
+                                <label htmlFor="token" className="bg-transparent color-white">Confirmation Code</label>
+                                <input
+                                    type="text"
+                                    id="token"
+                                    className="color-black bg-white font-inter"
+                                    placeholder="Enter the code sent to your email"
+                                    {...register("token", {
+                                        required: "Token not valid",
+                                        minLength: {
+                                            value: 6,
+                                            message: "Token not valid"
+                                        },
+                                        maxLength: {
+                                            value: 6,
+                                            message: "Token not valid"
+                                        }
+                                    })}
+                                />
+                                {errors.token && 
+                                    <ErrorMessageValidation>
+                                        { errors.token?.message }
+                                    </ErrorMessageValidation>
+                                }
+                            </div>
 
-                        <button className="btn-submit-form-users bg-black-medium color-gray font-inter" type="submit">
-                            Confirm Account
-                        </button>
-                    </div>
-                </form>
-            </section>
-        </main>
+                            <button className="btn-submit-form-users bg-black-medium color-gray font-inter" type="submit">
+                                Confirm Account
+                            </button>
+                        </div>
+                    </form>
+                </section>
+            </main>
+
+            <Toaster
+                richColors
+                position="bottom-right"
+            />
+        </>
     )
 }
 
